1bd2e854a332b9041975390dbb6e7f9df12dc7f7
Arnau Marcos Almansa <arnaumarcosalmansa@gmail.com>
2023-09-23 12:45:34 +0200

    pitcher y hq basicos

--- src/bot/UnitPlayer.java
+++ src/bot/UnitPlayer.java
@@ -1,12 +1,18 @@
 package bot;
 
+import aic2023.engine.Unit;
 import aic2023.user.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class UnitPlayer {
 
 	public void run(UnitController uc) {
 		/*Insert here the code that should be executed only at the beginning of the unit's lifespan*/
-		while (true){
+		while (true) {
 			/*Insert here the code that should be executed every round*/
 			UnitType type = uc.getType();
 			/*Get random direction*/
@@ -27,24 +33,97 @@ public class UnitPlayer {
 
 	void runHq(UnitController uc)
 	{
+		if (uc.getReputation() < 100) return;
 
+		Direction[] directions = randomDirections();
+		UnitType[] units = randomUnitTypes();
+
+		for (UnitType type : units) {
+			for (Direction dir : directions) {
+				if (uc.canRecruitUnit(type, dir)) {
+					uc.recruitUnit(type, dir);
+				}
+			}
+		}
 	}
 
 	void runPitcher(UnitController uc)
 	{
-
+		Location[] bases = uc.senseObjects(MapObject.BASE, 4);
+		if (bases.length == 0) {
+			moveAtRandom(uc);
+		} else {
+			moveTowards(uc, bases[0]);
+		}
 	}
 
 	void runBatter(UnitController uc)
 	{
-
+		moveAtRandom(uc);
 	}
 
 	void runCatcher(UnitController uc)
 	{
-
+		moveAtRandom(uc);
 	}
 
+
+	void moveTowards(UnitController uc, Location loc)
+	{
+		if (!uc.canMove()) return;
+
+		Location myLoc = uc.getLocation();
+		Direction dir = myLoc.directionTo(loc);
+
+		if (uc.canMove(dir)) {
+			uc.move(dir);
+		} else {
+			moveAtRandom(uc);
+		}
+	}
+
+	void moveAtRandom(UnitController uc)
+	{
+		if (!uc.canMove()) return;
+
+		Direction[] directions = randomDirections();
+		for (Direction dir : directions) {
+			if (uc.canMove(dir)) {
+				uc.move(dir);
+				break;
+			}
+		}
+	}
+
+	Direction[] randomDirections()
+	{
+		Direction[] dirs = Direction.values();
+
+		for (int i = 0; i < dirs.length; i++) {
+			int a = (int) (Math.random() * dirs.length);
+			int b = (int) (Math.random() * dirs.length);
+			Direction tmp = dirs[a];
+			dirs[a] = dirs[b];
+			dirs[b] = tmp;
+		}
+
+		return dirs;
+	}
+
+	UnitType[] randomUnitTypes()
+	{
+		UnitType[] types = UnitType.values();
+
+		for (int i = 0; i < types.length; i++) {
+			int a = (int) (Math.random() * types.length);
+			int b = (int) (Math.random() * types.length);
+			UnitType tmp = types[a];
+			types[a] = types[b];
+			types[b] = tmp;
+		}
+
+		return types;
+	}
 
 
 	/**
